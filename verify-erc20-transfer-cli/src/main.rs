855c66f0b158d23624e124a76ef08e94868f1e05
alresarena2021 <alres.arena2019@gmail.com>
2021-11-11 10:52:58 +0800

    feat: add comment to every line of code

--- verify-erc20-transfer-cli/src/main.rs
+++ verify-erc20-transfer-cli/src/main.rs
@@ -11,13 +11,17 @@ use web3::{
 };
 
 async fn _install_contract() -> web3::contract::Result<()> {
+    // init http protocol
     let http = web3::transports::Http::new("http://localhost:8545")?;
     let web3 = web3::Web3::new(http);
 
+    // owner address
     let my_account: H160 = hex!("d028D24F16A8893bd078259D413372aC01580769").into();
 
+    // erc20 binary
     let bytecode = include_str!("./build/ERC20.bin").trim_end();
 
+    // deploy erc20 to the node
     let _contract = Contract::deploy(web3.eth(), include_bytes!("./build/ERC20.abi"))?
         .confirmations(0)
         .options(Options::with(|opt| {
@@ -34,13 +38,14 @@ async fn _install_contract() -> web3::contract::Result<()> {
             my_account,
         )
         .await?;
-
+    // return
     Ok(())
 }
 
 #[tokio::main]
 async fn main() -> web3::contract::Result<()> {
-    let commandline_parser = App::new("ERC20 TRANSFER TOKEN VERFIER")
+    // command line parser
+    let commandline_parser = App::new("ERC20 TRANSFER TOKEN AND VERIFIER")
         .version("1.0")
         .author("alresarena2021@hov.co")
         .arg(
@@ -73,26 +78,34 @@ async fn main() -> web3::contract::Result<()> {
         )
         .get_matches();
 
-   
+    // init websockets protocol
     let web3 = web3::Web3::new(web3::transports::WebSocket::new("ws://localhost:8545").await?);
 
+    // owner address
     let my_account: H160 =
         Address::from_str(commandline_parser.value_of("owner").unwrap()).unwrap();
+
+    // recepient address
     let recepient: H160 =
         Address::from_str(commandline_parser.value_of("recepient").unwrap()).unwrap();
 
+    // contract address
     let contract_address: H160 =
         Address::from_str(commandline_parser.value_of("contract_address").unwrap()).unwrap();
+
+    // get contract context
     let contract_json = Contract::from_json(
         web3.eth(),
         contract_address,
         include_bytes!("./build/ERC20.abi"),
     )?;
 
+    // get balanceOf owner query
     let result = contract_json.query("balanceOf", (my_account,), None, Options::default(), None);
     let balance_of: U256 = result.await?;
     println!("Current Balance: {}", balance_of);
 
+    // create filter for transfer event
     let filter = FilterBuilder::default()
         .address(vec![contract_address])
         .topics(
@@ -106,11 +119,16 @@ async fn main() -> web3::contract::Result<()> {
         )
         .build();
 
+    // apply filter to web3
     let filter = web3.eth_filter().create_logs_filter(filter).await?;
 
+    // get filter stream
     let logs_stream = filter.stream(time::Duration::from_secs(1));
+
+    // pin log stream instance
     futures::pin_mut!(logs_stream);
 
+    // initiate transfer
     let data = contract_json
         .call(
             "transfer",
@@ -129,11 +147,15 @@ async fn main() -> web3::contract::Result<()> {
         )
         .await?;
 
+    // display transaction id
     println!("txn: {:?}", data);
 
+    // set timeout duration
     let duration = Duration::from_secs(60 * 30);
 
+    // set timeout when getting transfer event
     match tokio::time::timeout(duration, logs_stream.next()).await {
+        // if transfer event ok means it succeeded
         Ok(data) => {
             println!("{:?}", data);
             println!("Transaction Verified.");
@@ -146,6 +168,7 @@ async fn main() -> web3::contract::Result<()> {
             std::process::exit(0);
         }
 
+        // error on transfer
         Err(_) => {
             println!("Transaction Unverified.");
             std::process::exit(-1);
