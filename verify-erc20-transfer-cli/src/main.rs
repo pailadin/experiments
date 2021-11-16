c0df1e4eeada8b4a90b407efd209898da3dd9061
alresarena2021 <alres.arena2019@gmail.com>
2021-11-16 13:20:05 +0800

    feat: enhance erc29 verification

--- verify-erc20-transfer-cli/src/main.rs
+++ verify-erc20-transfer-cli/src/main.rs
@@ -6,8 +6,8 @@ use std::time::Duration;
 use web3::{
     contract::{Contract, Options},
     futures,
-    futures::StreamExt,
-    types::{Address, FilterBuilder, H160, U256},
+    futures::{ StreamExt},
+    types::{Address, FilterBuilder,  H160, H256, U256},
 };
 
 async fn _install_contract() -> web3::contract::Result<()> {
@@ -45,7 +45,7 @@ async fn _install_contract() -> web3::contract::Result<()> {
 #[tokio::main]
 async fn main() -> web3::contract::Result<()> {
     // command line parser
-    let commandline_parser = App::new("ERC20 TRANSFER TOKEN AND VERIFIER")
+    let commandline_parser = App::new("ERC20 TRANSFER TOKEN VERIFIER")
         .version("1.0")
         .author("alresarena2021@hov.co")
         .arg(
@@ -76,102 +76,89 @@ async fn main() -> web3::contract::Result<()> {
                 .takes_value(true)
                 .long("amount"),
         )
+        .arg(
+            Arg::with_name("transaction_address")
+                .help("transaction address")
+                .takes_value(true)
+                .required(true)
+                .long("transaction_address"),
+        )
         .get_matches();
 
     // init websockets protocol
     let web3 = web3::Web3::new(web3::transports::WebSocket::new("ws://localhost:8545").await?);
 
-    // owner address
-    let my_account: H160 =
-        Address::from_str(commandline_parser.value_of("owner").unwrap()).unwrap();
+    // convert transaction string to H256 data type
+    let transaction = H256::from_str(commandline_parser.value_of("transaction_address").unwrap()).unwrap();
 
-    // recepient address
-    let recepient: H160 =
-        Address::from_str(commandline_parser.value_of("recepient").unwrap()).unwrap();
+    // get receipt from OpenEthereum Node
+    let receipt_context = web3.eth().transaction_receipt(transaction).await?;
 
-    // contract address
-    let contract_address: H160 =
-        Address::from_str(commandline_parser.value_of("contract_address").unwrap()).unwrap();
+    // parse the transaction status
+    let status = receipt_context.and_then(|receipt| receipt.status);
 
-    // get contract context
-    let contract_json = Contract::from_json(
-        web3.eth(),
-        contract_address,
-        include_bytes!("./build/ERC20.abi"),
-    )?;
+    // evaluate transaction status
+    if status == Some(1.into()) {
+        println!("Transaction Verified.");
+    } else {
+        // owner address
+        let _my_account: H160 =
+            Address::from_str(commandline_parser.value_of("owner").unwrap()).unwrap();
 
-    // get balanceOf owner query
-    let result = contract_json.query("balanceOf", (my_account,), None, Options::default(), None);
-    let balance_of: U256 = result.await?;
-    println!("Current Balance: {}", balance_of);
+        // recepient address
+        let _recepient: H160 =
+            Address::from_str(commandline_parser.value_of("recepient").unwrap()).unwrap();
 
-    // create filter for transfer event
-    let filter = FilterBuilder::default()
-        .address(vec![contract_address])
-        .topics(
-            Some(vec![hex!(
-                "ddf252ad1be2c89b69c2b068fc378daa952ba7f163c4a11628f55a4df523b3ef"
+        // contract address
+        let contract_address: H160 =
+            Address::from_str(commandline_parser.value_of("contract_address").unwrap()).unwrap();
+
+    
+
+        // create filter for transfer event
+        let filter = FilterBuilder::default()
+            .address(vec![contract_address])
+            .topics(
+                Some(vec![hex!(
+                    "ddf252ad1be2c89b69c2b068fc378daa952ba7f163c4a11628f55a4df523b3ef"
+                )
+                .into()]),
+                None,
+                None,
+                None,
             )
-            .into()]),
-            None,
-            None,
-            None,
-        )
-        .build();
+            .build();
 
-    // apply filter to web3
-    let filter = web3.eth_filter().create_logs_filter(filter).await?;
+        // apply filter to web3
+        let filter = web3.eth_filter().create_logs_filter(filter).await?;
 
-    // get filter stream
-    let logs_stream = filter.stream(time::Duration::from_secs(1));
+        // get filter stream
+        let logs_stream = filter.stream(time::Duration::from_secs(1));
 
-    // pin log stream instance
-    futures::pin_mut!(logs_stream);
+        // pin log stream instance
+        futures::pin_mut!(logs_stream);
 
-    // initiate transfer
-    let data = contract_json
-        .call(
-            "transfer",
-            (
-                recepient,
-                U256::from(
-                    commandline_parser
-                        .value_of("amount")
-                        .unwrap()
-                        .parse::<u64>()
-                        .unwrap(),
-                ),
-            ),
-            my_account,
-            Options::default(),
-        )
-        .await?;
+        // set timeout duration
+        let duration = Duration::from_secs(60 * 30);
 
-    // display transaction id
-    println!("txn: {:?}", data);
+        // set timeout when getting transfer event
+        match tokio::time::timeout(duration, logs_stream.next()).await {
+            // if transfer event ok means it succeeded
+            Ok(data) => {
+               
+                println!("{:?}", data);
+                println!("Transaction Verified.");
+                std::process::exit(0);
+            }
 
-    // set timeout duration
-    let duration = Duration::from_secs(60 * 30);
-
-    // set timeout when getting transfer event
-    match tokio::time::timeout(duration, logs_stream.next()).await {
-        // if transfer event ok means it succeeded
-        Ok(data) => {
-            println!("{:?}", data);
-            println!("Transaction Verified.");
-
-            let result =
-                contract_json.query("balanceOf", (my_account,), None, Options::default(), None);
-            let balance_of: U256 = result.await?;
-            println!("New Balance: {}", balance_of);
-
-            std::process::exit(0);
-        }
-
-        // error on transfer
-        Err(_) => {
-            println!("Transaction Unverified.");
-            std::process::exit(-1);
+            // error on transfer
+            Err(_) => {
+                println!("Transaction Unverified.");
+                std::process::exit(-1);
+            }
         }
     }
+
+    Ok(())
+    
 }
