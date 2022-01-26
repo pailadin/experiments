4330c6beddb365dfe4a8051afa3092098ff4a6f5
Ralph Kevin Y. Casipe <ralphcasipe1@gmail.com>
2022-01-26 16:53:54 +0800

    test: add unit tests for addWorflow and executeWorkflow

--- flowstation/test/flow-station-workflow-module.ts
+++ flowstation/test/flow-station-workflow-module.ts
@@ -1,9 +1,13 @@
-import { expect } from "chai";
-import { AbiCoder } from "ethers/lib/utils";
+import chaiAsPromised from 'chai-as-promised';
+import { expect, use } from "chai";
 import { ethers } from "hardhat";
 
+use(chaiAsPromised);
+const { AbiCoder } = ethers.utils;
+
 describe.only("FlowStationWorkflowModule", function () {
-  const NO_ADDRESS = '0x0000000000000000000000000000000000000000';
+  const abiCoder = new AbiCoder();
+  const { AddressZero } = ethers.constants;
 
   it("should be deployed", async function () {
     const [owner] = await ethers.getSigners();
@@ -15,12 +19,30 @@ describe.only("FlowStationWorkflowModule", function () {
     await flowStation.deployed();
     
     expect(await flowStation.deployed()).to.equal(flowStation);
+  });
 
-    expect(await flowStation.safeWorkflows(owner.address, 0)).to.equals(NO_ADDRESS);
+  it('should have no workflows on the safe in the first place', async function () {
+    const [owner] = await ethers.getSigners();
 
+    const FlowStationWorkflowModule = await ethers.getContractFactory('FlowStationWorkflowModule', owner);
+
+    const flowStation = await FlowStationWorkflowModule.deploy();
+    
+    await flowStation.deployed();
+
+    expect(await flowStation.safeWorkflows(owner.address, 0)).to.equals(AddressZero);
     expect(await flowStation.safeWorkflowCount(owner.address)).to.equals(0);
+    expect(await flowStation.workflowDelegates(owner.address, 0, 0)).to.equals(AddressZero);
+  });
 
-    expect(await flowStation.workflowDelegates(owner.address, 0, 0)).to.equals(NO_ADDRESS);
+  it('should add a new workflow under a safe', async function () {
+    const [owner] = await ethers.getSigners();
+
+    const FlowStationWorkflowModule = await ethers.getContractFactory('FlowStationWorkflowModule', owner);
+
+    const flowStation = await FlowStationWorkflowModule.deploy();
+    
+    await flowStation.deployed();
 
     const selector = await flowStation.getSelector('greet(string)');
     const args = await flowStation.getByte('Hello, HOV!');
@@ -36,6 +58,32 @@ describe.only("FlowStationWorkflowModule", function () {
 
     await addWorkflowTx.wait();
 
-    expect(await flowStation.safeWorkflows(owner.address, 0)).to.not.equals(NO_ADDRESS);
+    expect(await flowStation.safeWorkflows(owner.address, 0)).to.not.equals(AddressZero);
+  });
+
+  it('should successfully execute the workflow', async function () {
+    const [owner] = await ethers.getSigners();
+
+    const FlowStationWorkflowModule = await ethers.getContractFactory('FlowStationWorkflowModule', owner);
+
+    const flowStation = await FlowStationWorkflowModule.deploy();
+    
+    await flowStation.deployed();
+    
+    const selector = await flowStation.getSelector('greet(string)');
+    const args = abiCoder.encode(['string'], ['Hello, world!']);
+
+    const addWorkflowTx = await flowStation.addWorkflow(
+      owner.address, 
+      [owner.address], 
+      [{ 
+        selector,
+        arguments: args,
+      }]
+    );
+
+    await addWorkflowTx.wait();
+    
+    expect(await flowStation.executeWorkflow(owner.address, 0)).to.not.eqls({});
   });
 });
