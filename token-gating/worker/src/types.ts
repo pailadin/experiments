0c20b5a3ec053f21b35a1ac9829e2e17a30cc0bc
alresarena2021 <alres.arena2019@gmail.com>
2022-02-07 06:32:32 +0800

    chore: remove localQueue symbol

--- token-gating/worker/src/types.ts
+++ token-gating/worker/src/types.ts
@@ -4,7 +4,6 @@ export const TYPES = {
   ORIGIN_URL: Symbol.for('ORIGIN_URL'),
   SCHEME: Symbol.for('SCHEME'),
   BASE_URL: Symbol.for('BASE_URL'),
-  localQueue: Symbol.for('workerLocalQueue'),
   CollectionRepository: Symbol.for('CollectionRepository'),
   OwnershipRepository: Symbol.for('OwnershipRepository'),
 };
