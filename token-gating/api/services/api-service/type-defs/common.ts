2d9bd14e5d96c4238e13865f0b73e5b283218eda
alresarena2021 <alres.arena2019@gmail.com>
2022-02-09 04:23:32 +0800

    chore: move type-defs to api-service

---
+++ token-gating/api/services/api-service/type-defs/common.ts
@@ -0,0 +1,39 @@
+import { gql } from 'apollo-server-koa';
+
+export default gql`
+scalar DateTime
+scalar URL
+scalar EmailAddress
+scalar ObjectID
+scalar Cursor
+
+interface Node {
+  id: ObjectID!
+}
+
+type PageInfo {
+  hasNextPage: Boolean!
+  endCursor: Cursor
+}
+
+type Edge {
+  cursor: Cursor!
+  node: Node!
+}
+
+type Connection {
+  totalCount: Int!
+  edges: [Edge!]!
+  pageInfo: PageInfo!
+}
+
+type Query {
+  node(id: ID!): Node
+}
+
+directive @deprecated(
+  reason: String = "Deprecated"
+) on OBJECT | FIELD_DEFINITION | INPUT_FIELD_DEFINITION
+directive @private on FIELD_DEFINITION
+directive @next on OBJECT | FIELD_DEFINITION
+`;
