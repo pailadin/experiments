d0fccf529f0ceb095eff42b863db4d6075a6d5f7
Ralph Kevin Y. Casipe <ralphcasipe1@gmail.com>
2022-02-09 18:10:51 +0800

    test: modify SimpleSwap tests

--- flowstation/hardhat/test/simple-swap/SimpleSwap.test.ts
+++ flowstation/hardhat/test/simple-swap/SimpleSwap.test.ts
@@ -1,7 +1,7 @@
 import { expect } from 'chai';
 import { ethers } from 'hardhat';
 
-describe('SimpleSwap', () => {
+describe('SimpleSwap', function () {
   beforeEach(async function () {
     const [ owner ] = await ethers.getSigners();
 
@@ -14,7 +14,7 @@ describe('SimpleSwap', () => {
   });
 
   it('should deploy successfully', async function () {
-    const actual = this.simpleSwap.deployed();
+    const actual = await this.simpleSwap.deployed();
 
     expect(actual).to.be.equal(this.simpleSwap);
   });
