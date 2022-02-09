cc93e0c45f4b3add66f146eb4d0180e54e3e9273
Ralph Kevin Y. Casipe <ralphcasipe1@gmail.com>
2022-02-09 18:16:13 +0800

    style(contract): change to UNLICENSED

--- flowstation/hardhat/contracts/test/TestToken.sol
+++ flowstation/hardhat/contracts/test/TestToken.sol
@@ -1,5 +1,5 @@
-// SPDX-License-Identifier: LGPL-3.0-only
-pragma solidity >=0.7.0;
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity <=0.8.11;
 
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
