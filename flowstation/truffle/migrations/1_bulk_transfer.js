03a137d594d27cd3be50f0d219ec9f2415125334
Ralph Kevin Y. Casipe <ralphcasipe1@gmail.com>
2022-02-10 12:10:28 +0800

    feat: update truffle folder

--- flowstation/truffle/migrations/1_bulk_transfer.js
+++
@@ -1,8 +0,0 @@
-const { deployTruffleContract } = require('@gnosis.pm/singleton-deployer-truffle');
-const BulkTransfer = artifacts.require("BulkTransfer");
-
-module.exports = function(deployer) {
-  deployer.then(async () => {
-    await deployTruffleContract(web3, BulkTransfer);
-  })
-};
--- flowstation/truffle/migrations/2_flow_station_workflow_module.js
+++
@@ -1,8 +0,0 @@
-const { deployTruffleContract } = require('@gnosis.pm/singleton-deployer-truffle');
-const FlowStationWorkflowModule = artifacts.require("FlowStationWorkflowModule");
-
-module.exports = function(deployer) {
-  deployer.then(async () => {
-    await deployTruffleContract(web3, FlowStationWorkflowModule);
-  })
-};
