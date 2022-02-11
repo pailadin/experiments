bec4c96973655fef3c34e4da702a9908eb5a09d2
alresarena2021 <alres.arena2019@gmail.com>
2022-02-11 11:28:40 +0800

    chore: remove mongo memory server on normal mode

--- token-gating/api/index.ts
+++ token-gating/api/index.ts
@@ -2,7 +2,6 @@
 
 import { Connection } from 'mongoose';
 
-import { MongoMemoryServer } from 'mongodb-memory-server';
 import { container } from './inversify.config';
 import logger from './library/logger';
 import { ApiService } from './services/api';
@@ -10,13 +9,6 @@ import { TYPES as GLOBAL_TYPES } from './types';
 
 async function start() {
   logger.info(process.env);
-
-  const mongod = await MongoMemoryServer.create();
-
-  container
-    .rebind(GLOBAL_TYPES.MONGODB_URI)
-    .toConstantValue(mongod.getUri());
-
   await container.get<ApiService>(GLOBAL_TYPES.ApiService).start();
 }
 
