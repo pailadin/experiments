3c15edfcb581cf5d70c152e4ef852c7e75b613b6
alresarena2021 <alres.arena2019@gmail.com>
2022-02-14 20:08:46 +0800

    test: add delete project test

--- token-gating/api/tests/project/mutation/delete-project.spec.ts
+++ token-gating/api/tests/project/mutation/delete-project.spec.ts
@@ -1,19 +1,28 @@
+import jsonWebToken from 'jsonwebtoken';
 import { Context as FixtureContext, setup, teardown } from '../../helpers/fixture';
-import { TYPES } from '../../../services/project/types';
+import { TYPES as PROJECT_TYPES } from '../../../services/project/types';
 import { container } from '../../../inversify.config';
 import ProjectRepository from '../../../services/project/repositories/project';
 import generateProject from '../../helpers/generate-project';
 import ObjectId from '../../../library/object-id';
+import AdminAccountRepository from '../../../services/account/repositories/admin-account';
+import { TYPES as GLOBAL_TYPES } from '../../../types';
+import generateAdminAccount from '../../helpers/generate-admin-account';
+import { TYPES as ACCOUNT_TYPES } from '../../../services/account/types';
 
 type Context = FixtureContext & {
   projectRepository: ProjectRepository;
+  adminAccountRepository: AdminAccountRepository;
+  secret: Buffer;
 };
 
 describe('Mutation.deleteProject', () => {
   beforeEach(async function (this: Context) {
     await setup.apply(this);
 
-    this.projectRepository = container.get<ProjectRepository>(TYPES.ProjectRepository);
+    this.adminAccountRepository = container.get<AdminAccountRepository>(ACCOUNT_TYPES.AdminAccountRepository);
+    this.projectRepository = container.get<ProjectRepository>(PROJECT_TYPES.ProjectRepository);
+    this.secret = container.get<Buffer>(GLOBAL_TYPES.JWT_SECRET);
   });
 
   afterEach(async function (this: Context) {
@@ -25,6 +34,22 @@ describe('Mutation.deleteProject', () => {
   });
 
   test('should delete project', async function (this: Context) {
+    const adminAccount = generateAdminAccount();
+    await this.adminAccountRepository.create({
+      id: adminAccount.id,
+      data: {
+        ...adminAccount,
+      },
+    });
+    const adminToken = jsonWebToken.sign(
+      { role: adminAccount.role },
+      this.secret,
+      {
+        subject: new ObjectId(adminAccount.id).toString(),
+        expiresIn: '30d',
+      },
+    );
+
     const project = generateProject();
     await this.projectRepository.create({
       id: project.id,
@@ -47,6 +72,7 @@ describe('Mutation.deleteProject', () => {
 
     const response = await this.request
       .post('/graphql')
+      .set('Authorization', `Bearer ${adminToken}`)
       .send({
         query, variables,
       });
